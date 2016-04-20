e3bb10baccd96aa89e57782953a134813a05e1f7
Jeremy Michaud <jeremy.michaud@heig-vd.ch>
2016-04-20 19:20:26 +0200

    smtp finished

--- 04-MailRobot/src/ch/heigvd/res/mailrobot/MailRobot.java
+++ 04-MailRobot/src/ch/heigvd/res/mailrobot/MailRobot.java
@@ -5,16 +5,23 @@ import java.io.IOException;
 import java.util.List;
 
 import ch.heigvd.res.mailrobot.config.ConfigurationManager;
+import ch.heigvd.res.mailrobot.mail.Message;
 import ch.heigvd.res.mailrobot.mail.Person;
+import ch.heigvd.res.mailrobot.smtp.ISmtpClient;
+import ch.heigvd.res.mailrobot.smtp.SmtpClient;
 
 public class MailRobot {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		System.out.println("coucou");
-		ConfigurationManager conf = new ConfigurationManager("config");
-		List<Person> pers = conf.getWitnessesToCC();
-		for(Person p : pers)
-			System.out.println(p.getFirstName() + " " + p.getLastName());
+		ISmtpClient smtp = new SmtpClient("127.0.0.1", 2525);
+		Message msg = new Message();
+		msg.setFrom("jeremy.michaud@heig-vd.ch");
+		msg.setBcc(new String[]{"bcc.aaa@heig-vd.ch"});
+		msg.setTo(new String[]{"to.aaa@heig-vd.ch"});
+		msg.setCc(new String[]{"cc.aaa@heig-vd.ch"});
+		msg.setBody("coucou");
+		msg.setSubject("blabla");
+		smtp.sendMessage(msg);
 	}
 
 }
